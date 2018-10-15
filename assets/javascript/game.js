46590cda721e1d62b0929186c2c800720a0caf3c
Robert Flanagan <robertflanagan95@gmail.com>
2018-10-15 00:36:05 -0400

    added crystal images, random value generation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,47 @@ $(document).ready(function() {
     var lossesDisplay = $("#losses");
     var scoreDisplay = $("#score");
 
+    var crystal1 = $("#crystal1");
+    var crystal2 = $("#crystal2");
+    var crystal3 = $("#crystal3");
+    var crystal4 = $("#crystal4");
+
+    // maximum possible goal
     var maxScore = 120;
+    // minimum possible goal
     var minScore = 19;
     var randomGoal;
 
-    // var maxValue = 12;
-    // var minValue = 1;
+    // max and min crystal values
+    var maxValue = 12;
+    var minValue = 1;
+
     var score = 0;
     var wins = 0;
     var losses = 0;
 
     function initializeGame() {
+        // reset the random goal and update the text
         randomGoal = Math.floor(Math.random()*(maxScore-minScore+1)+minScore);
         goalDisplay.text(randomGoal);
+
+        // reset the score and update the text
         score = 0;
         scoreDisplay.text(score);
+        winsDisplay.text(wins);
+        lossesDisplay.text(losses);
 
-        return score, randomGoal;
+        // generate new crystal values
+        crystal1.attr("value", Math.floor(Math.random()*(maxValue-minValue+1)+minValue));
+        console.log("Crystal 1: " + crystal1.attr("value"))
+        crystal2.attr("value", Math.floor(Math.random()*(maxValue-minValue+1)+minValue));
+        console.log("Crystal 2: " + crystal2.attr("value"))
+        crystal3.attr("value", Math.floor(Math.random()*(maxValue-minValue+1)+minValue));
+        console.log("Crystal 3: " + crystal3.attr("value"))
+        crystal4.attr("value", Math.floor(Math.random()*(maxValue-minValue+1)+minValue));
+        console.log("Crystal 4: " + crystal4.attr("value"))
+
+        // return score, randomGoal;
     }
 
     $(".crystal").on("click", function() {
